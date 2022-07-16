205a52ab1789f2bda9cdb0d674cd280251ea4dca
kaliston.sousa <kaliston.silvanio@gmail.com>
2022-07-16 17:08:48 -0300

    demo logg files configuration

--- src/main/java/br/com/springframe/logback/LogbackApplication.java
+++ src/main/java/br/com/springframe/logback/LogbackApplication.java
@@ -1,12 +1,17 @@
 package br.com.springframe.logback;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class LogbackApplication {
 
 	public static void main(String[] args) {
+		log.info("this is a info message");
+		log.warn("this is a warn message");
+		log.error("this is a error message");
 		SpringApplication.run(LogbackApplication.class, args);
 	}
 
