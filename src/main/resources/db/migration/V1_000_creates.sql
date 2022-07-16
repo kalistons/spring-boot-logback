08fea584b5c915b654aa13f9d7e3e41d0bb63e91
kaliston.sousa <kaliston.silvanio@gmail.com>
2022-07-16 15:09:22 -0300

    configuration files creates and inserts

---
+++ src/main/resources/db/migration/V1_000_creates.sql
@@ -0,0 +1,5 @@
+CREATE TABLE IF NOT EXISTS tb_users (
+    id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name varchar(40),
+    email varchar(50)
+);
---
+++ src/main/resources/db/migration/V2_000_inserts.sql
@@ -0,0 +1,7 @@
+INSERT INTO tb_users(name, email) VALUES ('NOAH', 'noah.nn@gmail.com');
+INSERT INTO tb_users(name, email) VALUES ('JOHN', 'john.hn@gmail.com');
+INSERT INTO tb_users(name, email) VALUES ('PEDRO', 'pedro.hn@gmail.com');
+INSERT INTO tb_users(name, email) VALUES ('YIAGO', 'yag.hn@gmail.com');
+INSERT INTO tb_users(name, email) VALUES ('JUDAS', 'jud.hn@gmail.com');
+INSERT INTO tb_users(name, email) VALUES ('PETTER', 'pet.hn@gmail.com');
+INSERT INTO tb_users(name, email) VALUES ('MIGUEL', 'mig.hn@gmail.com');
